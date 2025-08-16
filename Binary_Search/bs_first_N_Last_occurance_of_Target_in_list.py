d7952b1893259c38e839d098955114cb3fc16fb6
RajIIITR <101034828+RajIIITR@users.noreply.github.com>
2025-08-16 21:29:20 +0530

    1st N last Occurance of target in an array/list

---
+++ Binary_Search/bs_first_N_Last_occurance_of_Target_in_list.py
@@ -0,0 +1,43 @@
+from typing import List
+
+def firstOccurance(nums: List, target: int) -> int:
+    """
+    This function returns the index of the first occurrence of the target number in the given list of numbers
+    """
+    low = 0
+    high = len(nums) - 1
+    first = -1
+    while low<=high:
+        mid = (low + high) // 2
+        if nums[mid] == target:
+            first = mid
+            high = mid - 1
+        elif nums[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return first
+
+def LastOccurance(nums: List, target: int) -> int:
+    low = 0
+    high = len(nums) - 1
+    last = -1
+    while low<=high:
+        mid = (low + high) // 2
+        if nums[mid] == target:
+            last = mid
+            low = mid + 1
+        elif nums[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return last
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        return [firstOccurance(nums, target), LastOccurance(nums, target)]
+    
+if "__main__" == __name__:
+    nums = [5,7,7,8,8,10]
+    target = 8
+    print(Solution().searchRange(nums, target))
