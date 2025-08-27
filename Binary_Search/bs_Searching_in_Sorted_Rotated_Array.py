4e52d99b845ea3ceb86692a5f882db5896a9dfc1
RajIIITR <101034828+RajIIITR@users.noreply.github.com>
2025-08-27 17:06:35 +0530

    Rotated Array Problem using BS

--- Binary_Search/bs_Searching_in_Sorted_Rotated_Array.py
+++ Binary_Search/bs_Searching_in_Sorted_Rotated_Array.py
@@ -43,9 +43,25 @@ def S_Rotated_Array_Duplicate_Element(nums: list, target: int) -> bool:
     
     return False
 
+def find_min_Rotated_Sorted_Array(nums: list):
+    low = 0
+    high = len(nums) - 1
+    mn = 100
+    while low<=high:
+        mid = (low + high) // 2
+        if nums[low] < nums[mid]:
+            mn = min(mn, nums[low])
+            low = mid + 1
+        else:
+            mn = min(mn, nums[mid])
+            high = mid - 1
+    return mn
+
 if '__main__' == __name__:
     nums = [4, 5, 6, 7, 0, 1, 2]
     nums_D = [3, 1, 1]
+    nums_R = [4, 5, 6, 7, 0, 1, 2]
     target = 0
     print(S_Rotated_Array_Unique_Element(nums, target))
     print(S_Rotated_Array_Duplicate_Element(nums_D, target))
+    print(find_min_Rotated_Sorted_Array(nums_R))
