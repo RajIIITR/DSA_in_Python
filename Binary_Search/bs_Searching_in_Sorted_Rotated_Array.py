9db78d91b59c8c82eb1133339767dba5108ff5cf
RajIIITR <101034828+RajIIITR@users.noreply.github.com>
2025-08-27 17:12:30 +0530

    Rotated sorted array problem

--- Binary_Search/bs_Searching_in_Sorted_Rotated_Array.py
+++ Binary_Search/bs_Searching_in_Sorted_Rotated_Array.py
@@ -43,10 +43,10 @@ def S_Rotated_Array_Duplicate_Element(nums: list, target: int) -> bool:
     
     return False
 
-def find_min_Rotated_Sorted_Array(nums: list):
+def find_min_Rotated_Sorted_Array(nums: list) -> int:
     low = 0
     high = len(nums) - 1
-    mn = 100
+    mn = float("inf")
     while low<=high:
         mid = (low + high) // 2
         if nums[low] < nums[mid]:
@@ -57,6 +57,25 @@ def find_min_Rotated_Sorted_Array(nums: list):
             high = mid - 1
     return mn
 
+def find_Number_Of_Times_Array_Is_Rotated(nums: list) -> int:
+    low = 0
+    high = len(nums) - 1
+    mn = float("inf")
+    index = 0
+    while low<=high:
+        mid = (low + high) // 2
+        if nums[low] < nums[mid]:
+            if nums[low] < mn:
+                mn = nums[low]
+                index = low
+            low = mid + 1
+        else:
+            if nums[mid] < mn:
+                mn = nums[mid]
+                index = mid
+            high = mid - 1
+    return index
+
 if '__main__' == __name__:
     nums = [4, 5, 6, 7, 0, 1, 2]
     nums_D = [3, 1, 1]
@@ -65,3 +84,4 @@ if '__main__' == __name__:
     print(S_Rotated_Array_Unique_Element(nums, target))
     print(S_Rotated_Array_Duplicate_Element(nums_D, target))
     print(find_min_Rotated_Sorted_Array(nums_R))
+    print(find_Number_Of_Times_Array_Is_Rotated(nums_R))
