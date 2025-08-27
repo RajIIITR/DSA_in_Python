89418f36ec596ef9d955330cc8355ad4c145a173
RajIIITR <101034828+RajIIITR@users.noreply.github.com>
2025-08-27 10:13:32 +0530

    Sorted array search Q

--- Binary_Search/bs_Searching_in_Sorted_Rotated_Array.py
+++ Binary_Search/bs_Searching_in_Sorted_Rotated_Array.py
@@ -18,7 +18,34 @@ def S_Rotated_Array_Unique_Element(nums: list, target: int) -> int:
 
     return -1
 
+def S_Rotated_Array_Duplicate_Element(nums: list, target: int) -> bool:
+    low = 0
+    high = len(nums) - 1
+    mid = (high - low) // 2
+    while low <= high:
+        if nums[mid] == target:
+            return True
+        if nums[low] == nums[mid] == nums[high]:
+            low += 1
+            high -= 1
+            continue
+        
+        if nums[low] <= nums[mid]:
+            if nums[low] <= target <= nums[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        else:
+            if nums[mid] <= target <= nums[high]:
+                low = mid + 1
+            else:
+                high = mid - 1
+    
+    return False
+
 if '__main__' == __name__:
     nums = [4, 5, 6, 7, 0, 1, 2]
+    nums_D = [3, 1, 1]
     target = 0
     print(S_Rotated_Array_Unique_Element(nums, target))
+    print(S_Rotated_Array_Duplicate_Element(nums_D, target))
