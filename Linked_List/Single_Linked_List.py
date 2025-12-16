58b34aa787bb25f791f741d49fef11c7181df477
RajIIITR <101034828+RajIIITR@users.noreply.github.com>
2025-12-16 13:40:07 +0530

    Single Linked List

--- Linked_List/Single_Linked_List.py
+++ Linked_List/Single_Linked_List.py
@@ -17,9 +17,9 @@ class Node:
 
         if position == 0:
             new_node = Node(data)
-            new_node.next = self.next
-            self.next = new_node
-            return
+            new_node.next = self
+            self = new_node
+            return self
 
         while temp.next is not None and position > 1:
             temp = temp.next
@@ -31,6 +31,8 @@ class Node:
             temp.next = new_node
         else:
             print("Invalid position")
+
+        return self
 
     def delete(self, position):
         # Case 1: Delete head
@@ -57,6 +59,33 @@ class Node:
         del curr
 
         return self
+    
+    def search(self, data):
+        temp = self
+        while temp is not None:
+            if temp.data == data:
+                return True
+            temp = temp.next
+        return False
+    
+    def modify(self, data, newData):
+        temp = self
+        while temp is not None:
+            if temp.data == data:
+                temp.data = newData
+                return 
+            temp = temp.next
+        print("Data not found")
+
+    def reverse(self):
+        prev = None
+        curr =self
+        while curr is not None:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
 
 
 if __name__ == "__main__":
@@ -64,8 +93,16 @@ if __name__ == "__main__":
     head.next = Node(20)
     head.next.next = Node(30)
 
-    head.insert(25, 2)   # Insert after 20
+    head = head.insert(25, 3)   # Insert after 30
     head.traversal()
 
     head = head.delete(2)   # Delete first node
+    head.traversal()
+    print()
+
+    print(head.search(25))
+    head.modify(10, 35)
+    head.traversal()
+
+    head = head.reverse()
     head.traversal()
